4ef17e7601973f8edae20e675b05f88d5e17a968
nacir1i <taoufiqnaciri234@gmail.com>
2024-02-04 22:36:45 +0100

    create first layout components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
+<script setup lang="ts">
+import { view } from "./store";
+import Commands from "./components/Commands.vue";
+import Directory from "./components/Directory.vue";
+import File from "./components/File.vue";
+import { computed } from "vue";
+import { viewConfig } from "./utils";
+
+const config = computed(() => viewConfig(view.value.currentView));
+
+function updateVies() {
+  view.value.currentView === "DIRECTORY"
+    ? view.value.setCurrentView("FILE")
+    : view.value.setCurrentView("DIRECTORY");
+}
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline text-blue-400">
-    Hello world!
-  </h1>
+  <div
+    class="w-screen h-screen grid grid-cols-12 grid-rows-12 relative text-white"
+  >
+    <div :style="config" class="row-start-1 row-end-12 col-span-12">
+      <template v-if="view.currentView === 'DIRECTORY'">
+        <Directory />
+      </template>
+      <template v-else-if="view.currentView === 'FILE'">
+        <File />
+      </template>
+    </div>
+    <Commands />
+    <button @click="updateVies" class="absolute top-0 right-0">Click</button>
+  </div>
 </template>
---
+++ src/components/Commands.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const title = ref("Commands");
+</script>
+
+<template>
+  <div class="row-start-12 row-end-13 bg-[#222831] col-span-12">
+    <div class="w-full bg-[#606470]">
+      <h1>/current/directory/file.c - 0 lines (modified)</h1>
+    </div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
