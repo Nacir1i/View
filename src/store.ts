ac2c944bf5d3382241fb8f2abb271dd6bda30a3f
nacir1i <taoufiqnaciri234@gmail.com>
2024-02-09 01:10:28 +0100

    create dirrectory store

--- src/store.ts
+++ src/store.ts
@@ -3,12 +3,31 @@ import { View } from "./utils/interface";
 
 interface ViewStore {
   currentView: View;
-  setCurrentView: (view: View) => void;
+  setCurrentView(view: View): void;
+}
+
+interface DirectoryStore {
+  path: string;
+  setPath(path: string): Promise<void>;
 }
 
 export const view = ref<ViewStore>({
   currentView: "DIRECTORY",
   setCurrentView(view) {
     this.currentView = view;
+  },
+});
+
+export const directory = ref<DirectoryStore>({
+  path: "~",
+  async setPath(path) {
+    this.path = path;
+  },
+});
+
+export const directoryAbsolutePath = ref<DirectoryStore>({
+  path: "~",
+  async setPath(path) {
+    this.path = path;
   },
 });
