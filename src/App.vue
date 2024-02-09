4c36b3f86096e22c75ff0e12df637efcefeebc0c
nacir1i <taoufiqnaciri234@gmail.com>
2024-02-09 01:14:09 +0100

    init direcotry value

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <script setup lang="ts">
-import { view } from "./store";
+import { directory, view } from "./store";
 import Commands from "./components/Commands.vue";
 import Directory from "./components/Directory.vue";
 import File from "./components/File.vue";
 import { computed } from "vue";
 import { viewConfig } from "./utils";
+import { desktopDir } from "@tauri-apps/api/path";
 
 const config = computed(() => viewConfig(view.value.currentView));
 
-function updateVies() {
+async function setup() {
+  const initPath = await desktopDir();
+
+  console.log(initPath);
+
+  directory.value.setPath("./");
+}
+
+function updateViews() {
   view.value.currentView === "DIRECTORY"
     ? view.value.setCurrentView("FILE")
     : view.value.setCurrentView("DIRECTORY");
 }
+
+setup();
 </script>
 
 <template>
   <div
-    class="w-screen h-screen grid grid-cols-12 grid-rows-12 relative text-white"
+    class="w-screen h-screen grid grid-cols-12 grid-rows-12 relative text-white text-sm font-oxanium"
   >
-    <div :style="config" class="row-start-1 row-end-12 col-span-12">
+    <div :style="config" class="row-start-1 row-end-12 col-span-12 p-2">
       <template v-if="view.currentView === 'DIRECTORY'">
         <Directory />
       </template>
@@ -28,6 +39,6 @@ function updateVies() {
       </template>
     </div>
     <Commands />
-    <button @click="updateVies" class="absolute top-0 right-0">Click</button>
+    <button @click="updateViews" class="absolute top-0 right-0">Click</button>
   </div>
 </template>
