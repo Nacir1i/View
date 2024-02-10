4b4049e3529625bd842fcd23c85a9ebb0a461592
nacir1i <taoufiqnaciri234@gmail.com>
2024-02-10 03:01:33 +0100

    organize interface and create file related interfaces

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -4,6 +4,11 @@ export type ViewConfig = {
   color: string;
 };
 
+export interface ViewStore {
+  currentView: View;
+  setCurrentView(view: View): void;
+}
+
 export type View = "DIRECTORY" | "FILE";
 
 export type Targets = "dir" | "file";
@@ -16,4 +21,20 @@ export interface DirectoryEntity {
   created_at: string;
   size: number;
   is_dir: boolean;
+}
+
+export interface DirectoryStore {
+  path: string;
+  setPath(path: string): Promise<void>;
+}
+
+export interface FileEntity {
+  content: string;
+  path: string;
+  extension?: string;
+}
+
+export interface FileStore {
+  path: string;
+  setPath(path: string): Promise<void>;
 }
