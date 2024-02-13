d80f750999e4fa560c4b4e81fc6982d92ee7afe9
nacir1i <taoufiqnaciri234@gmail.com>
2024-02-13 20:47:08 +0100

    update interpreter

--- src/utils/interpreter.ts
+++ src/utils/interpreter.ts
@@ -1,7 +1,9 @@
 import { Actions, Targets } from "./interface";
+import { directory, directoryAbsolutePath, file, view } from "../store";
+import { invoke } from "@tauri-apps/api/tauri";
 
 export class Interpreter {
-  private ACTIONS: string[] = ["create", "delete", "rename", "change"] as const;
+  private ACTIONS: string[] = ["change", "open", "quit", "switch"] as const;
   private TARGET: string[] = ["dir", "file"] as const;
 
   constructor(private readonly input: string) {}
@@ -10,13 +12,13 @@ export class Interpreter {
     try {
       const split = this.input.split(" ");
 
-      if (split.length !== 3) {
-        throw new ParseError("Wrong number of arguments");
+      if (split.length > 3) {
+        throw new ParseError("Too many arguments (max: 3)");
       }
       if (!this.ACTIONS.includes(split[0])) {
         throw new ParseError(`Unknown action: ${split[0]}`);
       }
-      if (!this.TARGET.includes(split[1])) {
+      if (!this.TARGET.includes(split[1]) && split.length > 1) {
         throw new ParseError(`Unknown target: ${split[1]}`);
       }
 
@@ -38,17 +40,64 @@ export class Interpreter {
     console.log(`Executing command: ${action} ${target} ${arg}`);
 
     switch (action) {
-      case "create":
-        break;
-      case "delete":
-        break;
-      case "rename":
+      case "quit":
+        this.quitApp();
         break;
       case "change":
+        if (target === "dir") {
+          this.changeDirectory(arg);
+        }
+        break;
+      case "open":
+        if (target === "file") {
+          this.openFile(arg);
+        }
+        break;
+      case "switch":
+        this.changeView();
         break;
       default:
         break;
     }
+  }
+
+  changeView() {
+    const desiredView =
+      view.value.currentView === "DIRECTORY" ? "FILE" : "DIRECTORY";
+
+    view.value.setCurrentView(desiredView);
+  }
+
+  async changeDirectory(path: string) {
+    view.value.setCurrentView("DIRECTORY");
+
+    const jumpsArray = path.split("/");
+    const absolutPathArray = directoryAbsolutePath.value.path.split("/");
+
+    for (let i = 0; i < jumpsArray.length; i++) {
+      if (jumpsArray[i] === "..") {
+        absolutPathArray.pop();
+      } else {
+        absolutPathArray.push(jumpsArray[i]);
+      }
+    }
+
+    const newAbsolutPath = absolutPathArray.join("/");
+
+    await directory.value.setPath(newAbsolutPath);
+  }
+
+  async openFile(path: string) {
+    view.value.setCurrentView("FILE");
+
+    const newPath = `${directoryAbsolutePath.value.path}/${path}`;
+    console.log({ newPath });
+
+    await file.value.setPath(newPath);
+  }
+
+  async quitApp() {
+    await invoke<void>("quit_app");
   }
 }
 
